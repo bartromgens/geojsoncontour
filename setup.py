728e0bc700b58cb69752f617623b0b81e45e26ae
Bart Römgens <bart.romgens@gmail.com>
2016-05-03 02:09:04 +0200

    Add basic setup classifiers

--- setup.py
+++ setup.py
@@ -17,4 +17,24 @@ setup(
     zip_safe=False,
     test_suite='nose.collector',
     tests_require=['nose'],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
