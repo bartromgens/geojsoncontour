a7c81811cd06aa03039a70cd253ec2cc0583aa4d
urcyglu <urcyglu@gmail.com>
2016-09-06 22:40:48 +0200

    Added support for contourf export.

--- geojsoncontour/contour.py
+++ geojsoncontour/contour.py
@@ -1,8 +1,11 @@
-from matplotlib.colors import rgb2hex
-from geojson import Feature, LineString, FeatureCollection
-import geojson
+#!/usr/bin/python3.4
+# -*- encoding: utf-8 -*-
+"""Transform matplotlib.contour(f) to GeoJSON."""
 
+import geojson
 import numpy as np
+from matplotlib.colors import rgb2hex
+from geojson import Feature, LineString, Polygon, FeatureCollection
 
 
 def unit_vector(vector):
@@ -27,7 +30,8 @@ def angle(v1, v2):
 
 def contour_to_geojson(contour, geojson_filepath, contour_levels,
                        min_angle_deg=2,
-                       ndigits=5, unit='', stroke_width=5):
+                       ndigits=5, unit='', stroke_width=3):
+    """Transform matplotlib.contour to geojson."""
     collections = contour.collections
     total_points = 0
     total_points_original = 0
@@ -78,3 +82,31 @@ def contour_to_geojson(contour, geojson_filepath, contour_levels,
     dump = geojson.dumps(feature_collection, sort_keys=True)
     with open(geojson_filepath, 'w') as fileout:
         fileout.write(dump)
+
+
+def contourf_to_geojson(contourf, geojson_filepath, cf_levels, unit='',
+                        fill_opacity=.9, ndigits=3, min_angle_deg=None):
+    """Transform matplotlib.contourf to geojson."""
+    polygon_features = []
+    contourf_index = 0
+    for coll in contourf.collections:
+        color = coll.get_facecolor()
+        for path in coll.get_paths():
+            for coord in path.to_polygons():
+                coord = np.around(coord, ndigits) if ndigits else coord
+                polygon = Polygon(coordinates=[coord.tolist()])
+                fcolor = rgb2hex(color[0])
+                properties = {
+                    "stroke": fcolor,
+                    "stroke-width": 1,
+                    "stroke-opacity": 1,
+                    "fill": fcolor,
+                    "fill-opacity": fill_opacity,
+                    "title": "%.2f" % cf_levels[contourf_index] + ' ' + unit
+                }
+                feature = Feature(geometry=polygon, properties=properties)
+                polygon_features.append(feature)
+        contourf_index += 1
+    collection = FeatureCollection(polygon_features)
+    with open(geojson_filepath, 'w') as fileout:
+        geojson.dump(collection, fileout)
