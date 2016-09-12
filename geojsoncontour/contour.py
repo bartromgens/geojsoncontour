5822e08d0f7381db669f668423ffef01f79f5079
ucyo <cayoglu@me.com>
2016-09-12 15:11:22 +0200

    Refactoring of code.

--- geojsoncontour/contour.py
+++ geojsoncontour/contour.py
@@ -4,46 +4,10 @@
 
 import geojson
 import numpy as np
-from .helper import MP
 from matplotlib.colors import rgb2hex
 from geojson import Feature, LineString
 from geojson import Polygon, FeatureCollection
-
-
-def unit_vector(vector):
-    """Return the unit vector of the vector."""
-    return vector / np.linalg.norm(vector)
-
-
-def angle(v1, v2):
-    """Return the angle in radians between vectors 'v1' and 'v2'.
-
-    >>> angle_between((1, 0, 0), (0, 1, 0))
-    1.5707963267948966
-    >>> angle_between((1, 0, 0), (1, 0, 0))
-    0.0
-    >>> angle_between((1, 0, 0), (-1, 0, 0))
-    3.141592653589793
-    """
-    v1_u = unit_vector(v1)
-    v2_u = unit_vector(v2)
-    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
-
-
-def keep_high_angle(vertices, min_angle_deg):
-    """Keep vertices with angles higher then given minimum."""
-    accepted = []
-    v = vertices
-    v1 = v[1] - v[0]
-    accepted.append((v[0][0], v[0][1]))
-    for i in range(1, len(v) - 2):
-        v2 = v[i + 1] - v[i - 1]
-        diff_angle = np.fabs(angle(v1, v2) * 180.0 / np.pi)
-        if diff_angle > min_angle_deg:
-            accepted.append((v[i][0], v[i][1]))
-            v1 = v[i] - v[i - 1]
-    accepted.append((v[-1][0], v[-1][1]))
-    return np.array(accepted, dtype=vertices.dtype)
+from .helper import MP, angle, keep_high_angle, set_properties
 
 
 def contour_to_geojson(contour, geojson_filepath, contour_levels,
@@ -121,19 +85,6 @@ def contourf_to_geojson(contourf, geojson_filepath, contour_levels,
     with open(geojson_filepath, 'w') as fileout:
         geojson.dump(collection, fileout,
                      sort_keys=True, separators=(',', ':'))
-
-
-def set_properties(stroke_width, fcolor, fill_opacity, contour_levels,
-                   contourf_idx, unit):
-    """Set property values for Polygon."""
-    return {
-        "stroke": fcolor,
-        "stroke-width": stroke_width,
-        "stroke-opacity": 1,
-        "fill": fcolor,
-        "fill-opacity": fill_opacity,
-        "title": "%.2f" % contour_levels[contourf_idx] + ' ' + unit
-    }
 
 
 def contourf_to_multipolygeojson(contourf, geojson_filepath, contour_levels,
