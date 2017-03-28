4a08b4d848764ec8987b2a9247e2529af9d8cfe0
ucyo <cayoglu@me.com>
2017-03-28 19:54:10 +0200

    Cleanup

--- examples/example_netcdf.py
+++ examples/example_netcdf.py
@@ -19,7 +19,7 @@ def _matplot_preprocessing(da, *args, **kwargs):
 
 
 def _matplot(X, Y, Z, unit, name, contourtype='contourf',
-             cmap='viridis', levels=30, vmin=None, vmax=None):
+             cmap='viridis', levels=100, vmin=None, vmax=None):
     if not vmin:
         vmin = np.nanmin(Z)
     if not vmax:
@@ -36,16 +36,23 @@ def _matplot(X, Y, Z, unit, name, contourtype='contourf',
 
 
 def to_geojson(da, strdump=False, geojson_filepath='out.geojson',
-               multipolys=True):
+               multipolys=True, *args, **kwargs):
     temp = _matplot(**_matplot_preprocessing(da))
     gjc.to_geojson(contour=temp, strdump=strdump,
                    geojson_filepath=geojson_filepath,
-                   multipolys=multipolys)
+                   multipolys=multipolys, *args, **kwargs)
 
 
 if __name__ == '__main__':
     curdir = os.path.dirname(__file__)
     relpath = '../tests/sresa1b_ncar_ccsm3-example.nc'
     da = xr.open_dataset(os.path.join(curdir, relpath)).tas
-    gjson = os.path.join(curdir, 'out.geojson')
-    to_geojson(da, geojson_filepath=gjson)
+    if da.lon.units == 'degrees_east':
+        midpoint = int(da.lon.size/2.)
+        rot1 = da.shift(lon=-midpoint).dropna(dim='lon')
+        rot1['lon'] = rot1['lon'] - 180
+        rot2 = da.shift(lon=midpoint).dropna(dim='lon')
+        rot2['lon'] = rot2['lon'] - 180
+        da = xr.merge([rot1, rot2]).tas
+    gjson = os.path.join(curdir, 'out_contourf.geojson')
+    to_geojson(da, geojson_filepath=gjson, min_angle_deg=0, )
