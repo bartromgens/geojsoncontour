1623466f6ebf46e38e912e1e36e619335dd56276
Magnus Ulimoen <magnusu@met.no>
2024-12-31 13:13:37 +0100

    Fix contourf plotting

--- geojsoncontour/contour.py
+++ geojsoncontour/contour.py
@@ -6,7 +6,7 @@ from matplotlib.colors import rgb2hex
 from geojson import Feature, LineString
 from geojson import Polygon, FeatureCollection
 
-from .utilities.multipoly import MP, keep_high_angle, set_contourf_properties,get_contourf_levels
+from .utilities.multipoly import multi_polygon, keep_high_angle, set_contourf_properties,get_contourf_levels
 from .utilities.vertices import get_vertices_from_path
 
 
@@ -85,20 +85,20 @@ def contourf_to_geojson(contourf, geojson_filepath=None, min_angle_deg=None,
         variable_opacity = False
     polygon_features = []
     contourf_levels = get_contourf_levels(contourf.levels, contourf.extend)
-    for coll, level in zip(contourf.collections, contourf_levels):
-        color = coll.get_facecolor()
-        muli = MP(coll, min_angle_deg, ndigits)
-        polygon = muli.mpoly()
-        fcolor = rgb2hex(color[0])
-        if polygon.coordinates:
-            properties = set_contourf_properties(stroke_width, fcolor, fill_opacity, level, unit)
-            if geojson_properties:
-                properties.update(geojson_properties)
-            feature = Feature(geometry=polygon, properties=properties)
-            polygon_features.append(feature)
-            # print(len(polygon.coordinates))
-            if variable_opacity:
-                fill_opacity += opacity_increment
+    contourf_colors = contourf.get_facecolor()
+    for path, level, color in zip(contourf.get_paths(), contourf_levels, contourf_colors):
+        polygon = multi_polygon(path, min_angle_deg, ndigits)
+        if not polygon.coordinates:
+            continue
+        fcolor = rgb2hex(color)
+        properties = set_contourf_properties(stroke_width, fcolor, fill_opacity, level, unit)
+        if geojson_properties:
+            properties.update(geojson_properties)
+        feature = Feature(geometry=polygon, properties=properties)
+        polygon_features.append(feature)
+        # print(len(polygon.coordinates))
+        if variable_opacity:
+            fill_opacity += opacity_increment
     feature_collection = FeatureCollection(polygon_features)
     return _render_feature_collection(feature_collection, geojson_filepath, strdump, serialize)
 
